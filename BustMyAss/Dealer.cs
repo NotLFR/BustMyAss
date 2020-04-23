ede89bea511f6260dcba9f4ecfe73009e891bd4e
notLFR <elleffare@gmail.com>
2020-04-23 15:58:13 -0400

    Added hitting player and dealer methods

--- BustMyAss/Dealer.cs
+++ BustMyAss/Dealer.cs
@@ -1,12 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BustMyAss
 {
     class Dealer
     {
+        private readonly Cards deck;
+        public int DealerCount { get; set; }
+        public int PlayerCount { get; set; }
+
+        public Dealer()
+        {
+            deck = new Cards();
+            DealerCount = deck.getRandomCard();
+            PlayerCount = deck.getRandomCard();
+            Console.WriteLine($"Dealer starts with {DealerCount}");
+            Console.WriteLine($"You start with {PlayerCount}");
+        }
+
+        public void HitDealer()
+        {
+            Console.WriteLine($"Dealer hit with {DealerCount}...");
+            DealerCount += deck.getRandomCard();
+
+            if (DealerCount > 21)
+            {
+                Console.WriteLine($"Dealer busts with {DealerCount}!");
+            } else if (DealerCount >= 17)
+            {
+                Console.WriteLine("Dealer stays");
+            }
+        }
+
+        public void HitPlayer()
+        {
+            Console.WriteLine($"You hit with {PlayerCount}");
+            PlayerCount += deck.getRandomCard();
+
+            if (PlayerCount > 21)
+            {
+                Console.WriteLine("You busted");
+            }
+            else
+            {
+                Console.WriteLine($"You have {PlayerCount}");
+            }
+        }
+
+
     }
 }
