b24a2877f3d48d13fb9e65dc3cb4cdb6a1b65f5d
notLFR <elleffare@gmail.com>
2020-04-23 14:17:59 -0400

    Add project files.

---
+++ BustMyAss/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BustMyAss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world... \n \nThis is me trying to learn Blackjack");
+
+            Console.ReadKey();
+        }
+
+    }
+}
