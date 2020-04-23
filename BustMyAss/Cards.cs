de1c2a4b844ac63b23e331b5fd34de54f00d7b84
notLFR <elleffare@gmail.com>
2020-04-23 16:00:13 -0400

    Got rid of random seed

--- BustMyAss/Cards.cs
+++ BustMyAss/Cards.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BustMyAss
 {
     class Cards
     {
-        public int[] AllTheCards { get; } = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+        public List<int> AllTheCards { get; } = new List<int> {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+        private Random random = new Random();
+
+        public int getRandomCard()
+        {
+            return AllTheCards[random.Next(0, AllTheCards.Count - 1)];
+        }
     }
 }
